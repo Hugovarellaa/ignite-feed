be0ed59cca66c22f39bf324210af807e6e638836
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 <hugovarellaa@gmail.com>
2022-06-02 14:46:41 -0300

    Header

--- ignite-feed/src/components/Header/index.jsx
+++ ignite-feed/src/components/Header/index.jsx
@@ -1,9 +1,11 @@
 import styles from "./header.module.css";
 import "../styles/global.css";
+import igniteLogo from "../../assets/images/logo.svg"
 
 export function Header() {
   return (
     <header className={styles.header}>
+      <img src={igniteLogo} alt="Ignite Logo" className={styles.logo} />
       <strong>Ignite Feed</strong>
     </header>
   );
