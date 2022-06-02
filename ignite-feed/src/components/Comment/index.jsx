71c82851ccfa38013320bd4bba44c0e075c7d627
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 <hugovarellaa@gmail.com>
2022-06-02 19:34:22 -0300

    Component Avatar

--- ignite-feed/src/components/Comment/index.jsx
+++ ignite-feed/src/components/Comment/index.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "../Avatar";
 import styles from "./styles.module.css";
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/Hugovarellaa.png" />
+      <Avatar hasBorder={false} src="https://github.com/Hugovarellaa.png" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
@@ -15,12 +16,15 @@ export function Comment() {
                 Cercar de 1 hora atras
               </time>
             </div>
+
             <button title="Deletar comentarios">
               <Trash size={24} />
             </button>
           </header>
+
           <p>Muito bom Devon, Parabens!!</p>
         </div>
+
         <footer>
           <button>
             <ThumbsUp />
