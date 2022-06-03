4c520f0edbcedeca7b970e94b78d96c74f465af6
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 <hugovarellaa@gmail.com>
2022-06-03 16:36:29 -0300

    Commit initial project Typescript

---
+++ ignite-feed-typescript/src/components/Comment/index.jsx
@@ -0,0 +1,45 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import { useState } from "react";
+import { Avatar } from "../Avatar";
+import styles from "./styles.module.css";
+
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
+  }
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/Hugovarellaa.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Hugo Varela</strong>
+              <time title="11 de Maio as 08:00h" dateTime="2022-05-11 08:00:00">
+                Cercar de 1 hora atras
+              </time>
+            </div>
+
+            <button title="Deletar comentarios" onClick={handleDeleteComment}>
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
