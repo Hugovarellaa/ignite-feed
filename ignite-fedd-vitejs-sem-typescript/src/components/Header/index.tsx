850f29621f6135b6cdd27e60cd4ed72df59d658f
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 <hugovarellaa@gmail.com>
2022-08-22 15:25:31 -0300

    Confirando fontes e estilos globais

---
+++ ignite-fedd-vitejs-sem-typescript/src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./Header.styles.module.css"
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <h1>header</h1>
+    </header>
+  )
+}
