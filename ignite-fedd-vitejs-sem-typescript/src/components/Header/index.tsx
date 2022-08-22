8ab243c60f8e469dfb7503b293bf79aa9930a33c
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 <hugovarellaa@gmail.com>
2022-08-22 15:50:53 -0300

    Logo header

--- ignite-fedd-vitejs-sem-typescript/src/components/Header/index.tsx
+++ ignite-fedd-vitejs-sem-typescript/src/components/Header/index.tsx
@@ -1,9 +1,12 @@
+import logoImg from "../../images/Logo.svg"
 import styles from "./Header.styles.module.css"
+
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
-      <h1>header</h1>
+      <img src={logoImg} alt="" />
+      <h2>Ignite Feed</h2>
     </header>
   )
 }
