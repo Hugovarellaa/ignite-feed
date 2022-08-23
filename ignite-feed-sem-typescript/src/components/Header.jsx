ba17455266657451b399bae6ced2b2cec7065bd7
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 <hugovarellaa@gmail.com>
2022-08-23 14:48:04 -0300

    Header da aplicação finalizado

--- ignite-feed-sem-typescript/src/components/Header.jsx
+++ ignite-feed-sem-typescript/src/components/Header.jsx
@@ -1,9 +1,12 @@
+import logoImg from "../images/Logo.svg"
 import styles from "./Header.module.css"
 
-export function Header (){
+
+export function Header() {
   return (
     <div className={styles.headerContainer}>
-      header
+      <img src={logoImg} alt="" />
+      <span>Ignite Feed</span>
     </div>
   )
 }
