3f4bd52c3bdb446aa39da49986aa38514010ebc1
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 <hugovarellaa@gmail.com>
2022-08-23 15:21:46 -0300

    Sidebar finalizada

--- ignite-feed-sem-typescript/src/App.jsx
+++ ignite-feed-sem-typescript/src/App.jsx
@@ -1,4 +1,6 @@
+import styles from "./App.module.css";
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
 import "./styles/global.css";
 
@@ -6,6 +8,16 @@ export function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.container}>
+        <Sidebar />
+        <main>
+          <h1>Post</h1>
+          <h1>Post</h1>
+          <h1>Post</h1>
+          <h1>Post</h1>
+        </main>
+      </div>
     </div>
   );
-}
+} 
--- ignite-feed-sem-typescript/src/components/Header.jsx
+++ ignite-feed-sem-typescript/src/components/Header.jsx
@@ -5,7 +5,7 @@ import styles from "./Header.module.css"
 export function Header() {
   return (
     <div className={styles.headerContainer}>
-      <img src={logoImg} alt="" />
+      <img src={logoImg} alt="" className={styles.logo} />
       <span>Ignite Feed</span>
     </div>
   )
