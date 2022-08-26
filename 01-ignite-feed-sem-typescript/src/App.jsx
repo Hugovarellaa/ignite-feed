fb43c459801421f3867635a236e1310af87cf552
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 <hugovarellaa@gmail.com>
2022-08-26 09:35:05 -0300

    Layout Sidebar

--- 01-ignite-feed-sem-typescript/src/App.jsx
+++ 01-ignite-feed-sem-typescript/src/App.jsx
@@ -1,10 +1,18 @@
-import { Header } from "./components/Header"
-import "./styles/global.css"
+import styles from "./App.module.css";
+
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import "./styles/global.css";
 
 export function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.appContainer}>
+        <Sidebar />
+        <main>Post</main>
+      </div>
     </div>
   )
 }
